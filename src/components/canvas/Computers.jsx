bd788f591a55b9b298da88ea7f5aab6e00f989ae
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-12-05 15:00:47 +0200

    add gltf imgs

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,6 +6,7 @@ import CanvasLoader from "../Loader";
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
+
   return (
     <mesh>
       <hemisphereLight intensity={2} skyColor="white" groundColor="black" />
