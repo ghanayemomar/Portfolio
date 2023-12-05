a3c34eb5044a34f95319e330ba0122efa4bbd51b
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-12-05 23:31:28 +0200

    prevent resize

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -93,7 +93,7 @@ const Contact = () => {
               value={form.message}
               onChange={handleChange}
               placeholder="What do you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
+              className="bg-tertiary resize-none py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium"
             />
           </label>
           <button
