b99d376f2d981f6153677fa5c001d733b6188587
ghanayemomar <o.ghanaiem@student.aaup.edu>
2026-01-06 11:35:11 +0200

    Configure Vite deploy for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"http://ghanayemomar.github.io/Portfolio/",
-})
+  base: "/", // 👈 important for custom domain
+});
