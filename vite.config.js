f661df288eb14ed001c79f42e0c820a18263f26d
ghanayemomar <o.ghanaiem@student.aaup.edu>
2026-01-06 11:47:16 +0200

    Set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 important for custom domain
+  base: "/Portfolio/", // 👈 important for custom domain
 });
