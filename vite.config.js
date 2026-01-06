8de1d0e913c5c1fbbe4fae844ff50e3fec74112e
ghanayemomar <o.ghanaiem@student.aaup.edu>
2026-01-06 12:13:08 +0200

    Fix base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/", // 👈 important for custom domain
+  base: "/", // 👈 important for custom domain
 });
